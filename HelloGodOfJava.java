c84ece4f9459c154a2207f5d0b9cce66642b2167
Uigyeol Kim <uigyeolgim@gmail.com>
2024-10-24 23:38:06 +0900

    reviewing the basic of java after the setting

---
+++ HelloGodOfJava.java
@@ -0,0 +1,15 @@
+public class HelloGodOfJava {
+    public static void main(String[] args) {
+        int x=1;
+        int y=2;
+        int result = x + y;
+        System.out.println(result);
+
+        int hour=3;
+        int minute=5;
+        System.out.println(hour+"시간"+minute+"분");
+
+        int totalMinute=(hour*60)+minute;
+        System.out.println("총"+totalMinute+"분");
+    }
+}
